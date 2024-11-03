bc5b723328b4714a9703735f9cfb2e7e7129c782
Jeffrey Valdez <jeff.valdez321@outlook.com>
2024-11-03 00:55:13 -0400

    finished home section

--- frontend/src/components/home/Section.jsx
+++ frontend/src/components/home/Section.jsx
@@ -1,13 +1,23 @@
+import Button from "../button/Button";
+
 const Section = () => {
   return (
-    <section>
+    <section className="home-section">
       <div>
-        <img src="" alt="" />
+        <img
+          src="../../../public/close-up-food-lover-taking-pictures-meal-1.jpg"
+          alt="close up food over taking pictures meal"
+        />
       </div>
-      <div>
-        <h2></h2>
-        <p></p>
-        <button></button>
+      <div className="home-section-right">
+        <h2>
+          Share Your <span>Recipes</span>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          vitae enim pharetra, venenatis nunc eget, finibus est. Proin velit
+        </p>
+        <Button className="btn primary medium-btn">Create New Recipe</Button>
       </div>
     </section>
   );
