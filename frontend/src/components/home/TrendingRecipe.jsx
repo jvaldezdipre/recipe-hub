ad161c985e93c3725d29f095c9b10d7e22abed1d
Jeffrey Valdez <jeff.valdez321@outlook.com>
2024-11-03 19:10:10 -0500

    Started working on the Recipe card

--- frontend/src/components/home/TrendingRecipe.jsx
+++ frontend/src/components/home/TrendingRecipe.jsx
@@ -1,9 +1,15 @@
+import RecipeCard from "../recipe-card/RecipeCard";
+
 const TrendingRecipe = () => {
   return (
     <div className="home-trending-recipe">
       <h3>Trending Recipe</h3>
-      <p>View more</p>
-      <div className="home-trending-recipe-cards-grid"></div>
+      <p className="home-trending-recipe-view-more">View more</p>
+      <div className="home-trending-recipe-cards-container">
+        <div className="home-trending-recipe-cards-grid">
+          <RecipeCard />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/recipe-card/RecipeCard.jsx
@@ -0,0 +1,25 @@
+const RecipeCard = () => {
+  return (
+    <div className="recipe-card">
+      <div className="recipe-img-container">
+        <img src="" alt="" />
+      </div>
+      <div className="recipe-description-container">
+        <div className="rating"></div>
+        <p>Recipe Name</p>
+        <div className="recipe-description-container-bottom">
+          <div className="user-img">
+            <img src="" alt="" />
+          </div>
+          <p>User Name</p>
+          <div className="recipe-card-calories-container">
+            <div className="svg"></div>
+            <p>Calories</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
