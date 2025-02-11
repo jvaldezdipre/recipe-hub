785c639b5ebe2b31901f3c95e2beff1a47249847
Jeffrey Valdez <jeff.valdez321@outlook.com>
2025-02-11 14:04:29 -0500

    commit

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf6ec072ddc9c1bba699be2fd5aaa52b08eded3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00bf6748bfcfec0422f2152d6200eaede24c2288")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
